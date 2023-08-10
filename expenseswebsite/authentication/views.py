03cfc59d4d72a8110909f5d693ded49110bd4839
ncore-x <ncore@internet.ru>
2023-08-10 19:10:30 +1100

    configure the messages alert system

--- expenseswebsite/authentication/views.py
+++ expenseswebsite/authentication/views.py
@@ -4,6 +4,7 @@ import json
 from django.http import JsonResponse
 from django.contrib.auth.models import User
 from validate_email import validate_email
+from django.contrib import messages
 
 
 class EmailValidationView(View):
@@ -25,10 +26,18 @@ class UsernameValidationView(View):
             return JsonResponse({'username_error': 'username should only contain alphanumeric characters'}, status=400)
         if User.objects.filter(username=username).exists():
             return JsonResponse({'username_error': 'sorry username is use, choose another one'}, status=409)
-
         return JsonResponse({'username_valid': True})
 
 
 class RegistrationView(View):
     def get(self, request):
+        return render(request, 'authentication/register.html')
+
+    def post(self, request):
+
+        messages.success(request, 'Success whatsapp success')
+        messages.warning(request, 'Success whatsapp warning')
+        messages.info(request, 'Success whatsapp info')
+        messages.error(request, 'Success whatsapp err')
+
         return render(request, 'authentication/register.html')
--- expenseswebsite/expenseswebsite/settings.py
+++ expenseswebsite/expenseswebsite/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+from django.contrib import messages
 import os
 from pathlib import Path
 
@@ -128,3 +129,7 @@ STATIC_ROOT=os.path.join(BASE_DIR, 'static')
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MESSAGE_TAGS={
+    messages.ERROR: 'danger'
+}
