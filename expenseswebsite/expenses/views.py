6a91062b6fe5e3fa1fc33ce766cc916d40168978
ncore-x <ncore@internet.ru>
2023-08-23 18:01:05 +1100

    pagination in django

--- expenseswebsite/expenses/views.py
+++ expenseswebsite/expenses/views.py
@@ -2,15 +2,20 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from .models import Category, Expense
 from django.contrib import messages
+from django.contrib.auth.models import User
+from django.core.paginator import Paginator
 
 
 @login_required(login_url='/authentication/login')
 def index(request):
     categories = Category.objects.all()
     expenses = Expense.objects.filter(owner=request.user)
-
+    paginatro = Paginator(expenses, 2)
+    page_number = request.GET.get('page')
+    page_obj=Paginator.get_page(paginator, page_number)
     context = {
-        'expenses': expenses
+        'expenses': expenses,
+        'page_number': page_ojb
     }
     return render(request, 'expenses/index.html', context)
 
@@ -74,11 +79,11 @@ def expense_edit(request, id):
         Expense.objects.create(owner=request.user, amount=amount, date=date,
                                category=category, description=description)
 
-        expense.owner=request.user
-        expense.amount=amount
-        expense.date=date
-        expense.category=category
-        expense.description=description
+        expense.owner = request.user
+        expense.amount = amount
+        expense.date = date
+        expense.category = category
+        expense.description = description
 
         expense.save()
         expense.success(request, 'Expense update successfully')
@@ -87,7 +92,7 @@ def expense_edit(request, id):
 
 
 def delete_expense(request, id,):
-    expense=Expense.objects.get(pk=id)
+    expense = Expense.objects.get(pk=id)
     expense.delete()
     messages.success(request, 'Expense removed')
     return redirect('expenses')
