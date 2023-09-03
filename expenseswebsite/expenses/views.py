372668be8aab3625a34df987d4987dba0c7f6c5d
ncore-x <ncore@internet.ru>
2023-09-04 01:20:37 +1100

    fixing the form submission error

--- expenseswebsite/expenses/views.py
+++ expenseswebsite/expenses/views.py
@@ -7,12 +7,11 @@ from django.core.paginator import Paginator
 import json
 from django.http import JsonResponse
 from userpreferences.models import UserPreference
-
+import datetime
 
 def search_expenses(request):
     if request.method == 'POST':
         search_str = json.loads(request.body).get('searchText')
-
         expenses = Expense.objects.filter(
             amount__istartswith=search_str, owner=request.user) | Expense.objects.filter(
             date__istartswith=search_str, owner=request.user) | Expense.objects.filter(
@@ -26,10 +25,13 @@ def search_expenses(request):
 def index(request):
     categories = Category.objects.all()
     expenses = Expense.objects.filter(owner=request.user)
-    paginator = Paginator(expenses, 2)
+    paginator = Paginator(expenses, 5)
     page_number = request.GET.get('page')
-    page_obj = Paginator.get_page(paginator, page_number)
-    currency = UserPreference.objects.get(user=request.user).currency
+    page_obj = paginator.get_page(page_number)  # Исправлено
+    try:
+        currency = UserPreference.objects.get(user=request.user).currency
+    except UserPreference.DoesNotExist:
+        currency = 'USD'  # Установите значение по умолчанию
     context = {
         'expenses': expenses,
         'page_obj': page_obj,
@@ -46,7 +48,6 @@ def add_expense(request):
         'values': request.POST
     }
     if request.method == 'GET':
-
         return render(request, 'expenses/add_expense.html', context)
 
     if request.method == 'POST':
@@ -60,26 +61,25 @@ def add_expense(request):
         category = request.POST['category']
 
         if not description:
-            messages.error(request, 'Description is required')
+            messages.error(request, 'description is required')
             return render(request, 'expenses/add_expense.html', context)
 
-        Expense.objects.create(owner=request.user, amount=amount, date=date,
-                               category=category, description=description)
+        Expense.objects.create(owner=request.user, amount=amount, date=date,category=category, description=description)
         messages.success(request, 'Expense saved successfully')
 
         return redirect('expenses')
 
 
+@login_required(login_url='/authentication/login')
 def expense_edit(request, id):
     expense = Expense.objects.get(pk=id)
     categories = Category.objects.all()
     context = {
         'expense': expense,
         'values': expense,
-        'categories': category,
+        'categories': categories
     }
     if request.method == 'GET':
-
         return render(request, 'expenses/edit-expense.html', context)
     if request.method == 'POST':
         amount = request.POST['amount']
@@ -92,25 +92,22 @@ def expense_edit(request, id):
         category = request.POST['category']
 
         if not description:
-            messages.error(request, 'Description is required')
-            return render(request, 'expenses/add_expense.html', context)
-
-        Expense.objects.create(owner=request.user, amount=amount, date=date,
-                               category=category, description=description)
+            messages.error(request, 'description is required')
+            return render(request, 'expenses/edit-expense.html', context)
 
         expense.owner = request.user
         expense.amount = amount
-        expense.date = date
+        expense. date = date
         expense.category = category
         expense.description = description
 
         expense.save()
-        expense.success(request, 'Expense update successfully')
+        messages.success(request, 'Expense updated  successfully')
 
         return redirect('expenses')
 
 
-def delete_expense(request, id,):
+def delete_expense(request, id):
     expense = Expense.objects.get(pk=id)
     expense.delete()
     messages.success(request, 'Expense removed')
